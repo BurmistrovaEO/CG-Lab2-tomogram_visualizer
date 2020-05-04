381ecf3d4b19887eff43ea2ed61abe71f4c3264a
BurmistrovaEO <tigrettokat@mail.ru>
2020-05-04 03:47:09 +0300

    image appears

---
+++ Bourmistrova__tomogram_visualizer/View.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Bourmistrova_tomogram_visualizer
+{
+    class View
+    {
+        int clamp(int val, int min, int max)
+        {
+            if (val < min)
+                return min;
+            if (val > max)
+                return max;
+            return val;
+        }
+        public void SetupView(int width, int height)
+        {
+            GL.ShadeModel(ShadingModel.Smooth); //интерполирование цветов
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, Bin.X, 0, Bin.Y, -1, 1);
+            GL.Viewport(0, 0, width, height);
+        }
+        public Color TransferFunction(short value)
+        {
+            int min = 0;
+            int max = 2000;
+            int newVal = clamp((value - min) * 255 / (max - min), 0, 255);
+            return Color.FromArgb(255, newVal, newVal, newVal);
+        }
+        public void DrawQuads(int layerNumber)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Begin(PrimitiveType.Quads);
+            for (int x_coord = 0; x_coord < Bin.X - 1; x_coord++)
+                for (int y_coord = 0; y_coord < Bin.Y - 1; y_coord++)
+                {
+                    short value;
+                    //1 вершина
+                    value = Bin.array[x_coord + y_coord * Bin.X
+                                        + layerNumber * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(value));
+                    GL.Vertex2(x_coord, y_coord);
+                    //2 вершина
+                    value = Bin.array[x_coord + (y_coord + 1) * Bin.X
+                                        + layerNumber * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(value));
+                    GL.Vertex2(x_coord, y_coord + 1);
+                    //3 вершина
+                    value = Bin.array[x_coord + 1 + (y_coord + 1) * Bin.X
+                                        + layerNumber * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(value));
+                    GL.Vertex2(x_coord + 1, y_coord + 1);
+                    //4 вершина
+                    value = Bin.array[x_coord + 1 + y_coord * Bin.X
+                                        + layerNumber * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(value));
+                    GL.Vertex2(x_coord + 1, y_coord);
+                }
+            GL.End();
+        }
+
+    }
+}
